b969b6ad6b3a65911b706f03666a466477a8bc61
Shahid <shahid.sarker@gmail.com>
2020-07-01 18:49:56 -0400

    notes in Table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Table } from "react-bootstrap";
 import "./App.css";
 
 import {
@@ -45,13 +46,18 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map((note) => (
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      ))}
-    </ul>
+    <Table striped>
+      <tbody>
+        {notes.map((note) => (
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>{note.content}</Link>
+            </td>
+            <td>{note.user}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   </div>
 );
 
@@ -127,7 +133,7 @@ const App = () => {
     : null;
 
   return (
-    <div>
+    <div className="container">
       <div>
         <Link style={padding} to="/">
           home
